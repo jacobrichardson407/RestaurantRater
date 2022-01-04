1491b6edc8807e047c579795f07d5fbf194451ac
Jacob Richardson <jacobrichardson407@gmail.com>
2022-01-04 17:15:52 -0500

    Added Create View and Controller

--- RestaurantRater/Controllers/RestaurantController.cs
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -11,10 +12,42 @@ namespace RestaurantRater.Controllers
     {
         private RestaurantDbContext _db = new RestaurantDbContext();
 
-        // GET: Restaurant
+        // GET: Restaurant/Index
         public ActionResult Index()
         {
             return View(_db.Restaurants.ToList());
+        }
+        // GET: Restaurant/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+        // POST: Restaurant/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Restaurants.Add(restaurant);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(restaurant);
+        }
+        // DELETE: Restaurant/Delete/{id}
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Restaurant restaurant = _db.Restaurants.Find(id);
+            if (restaurant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(restaurant);
         }
     }
 }
